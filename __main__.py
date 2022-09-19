498de3b41f911a07b0d9aa6e68b0649ea450c6ec
Mounish713 <104339696+Mounish713@users.noreply.github.com>
2022-09-19 12:23:50 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,104 +1,174 @@
 import pulumi
-import pulumi_aws as aws
+from pulumi_azure_native import storage
+from pulumi_azure_native import resources
+from pulumi_azure_native import network
+from pulumi_azure_native import containerservice
+from pulumi_azure_native import compute
+import pulumi_azure_native as azure_native
 
-config = pulumi.Config()
-key_name = config.get('keyName')
-public_key = config.get('publicKey')
 
-def decode_key(key):
-    try:
-        key = base64.b64decode(key.encode('ascii')).decode('ascii')
-    except:
-        pass
-    if key.startswith('-----BEGIN RSA PRIVATE KEY-----'):
-        return key
-    return key.encode('ascii')
+prefix_name = "pulumiAKS"
 
-private_key = config.require_secret('id.pem').apply(decode_key)
+vnet_ip_range = "192.168.0.0/16"
+vm_ip_range = "192.168.16.0/24"
 
-virtualprivatecloud = aws.ec2.Vpc("devopsjunc-vpc",
-cidr_block="10.0.0.0/16")
+ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDfLeEbljcts7f6jxEnWvdfjAMoIL/yBY8DE4GgVJlWxg91sMAiq8BisxfJblu5GAq2MeHvgx3K8DS5RV49JpZApeAUEImiC3jqrvE7qF7YJg2fU5DITNavonUsvAxXz+64AS/o5MVv39zsjMbQrP9Chj5daO++n7gJfB7WJgNFhnNcl7Z4easj8CcsDlpfWx7CKwAacKtowNuDDJtcY9YuNloqVG0E6U3bSmnVGn/+uHkMvPd2sOLytwvj7aQ46AQoYWTUcqlu/67NQhi8kzPo4HKOI1J3j/ZczqIrxhfqxsj3+PVlst+oPnL74/dk6IjICpl8iIHNCVYe70lQX4DmTocpyPqYVgeCNqrG8ivhIeUOh2zfbKUyHZThYuDVsrCiJf93VAnfiy0jbpP6YEjWw2slvMsTwVPmc3ri4GtzQsix32JBQJ7AodUF+SUBQRCZASo4FbgtPN2JVVOHQZaBI9lza2E/s975N+ZWaAY7vIekqNChJkA963pdLjfI0LU= generated-by-azure"
 
-privatesubnet = aws.ec2.Subnet("devopsjunc-private-subnet",
-    vpc_id=virtualprivatecloud.id,
-    cidr_block="10.0.1.0/24",
-    map_public_ip_on_launch=False,
-    tags={
-        "Name": "devopsjunc-private-subnet",
-    })
+# Create an Azure Resource Group
+resource_group = resources.ResourceGroup(
+    prefix_name+"-rg",
+    resource_group_name=(prefix_name+"-rg"))
 
-publicsubnet = aws.ec2.Subnet("devopsjunc-public-subnet",
-    vpc_id=virtualprivatecloud.id,
-    cidr_block= "10.0.0.0/24",
-    map_public_ip_on_launch=True,
-    tags={
-        "Name": "devopsjunc-public-subnet",
-    })
+#Network security Groups
+nsg = network.NetworkSecurityGroup(
+    resource_name=(prefix_name+"-nsg"),
+    network_security_group_name=(prefix_name+"-nsg"),
+    resource_group_name=resource_group.name,
+    security_rules=[network.SecurityRuleArgs(
+        access="Allow",
+        destination_address_prefix="*",
+        destination_port_range="22",
+        direction="Inbound",
+        name="Allow-SSH",
+        priority=130,
+        protocol="*",
+        source_address_prefix="*",
+        source_port_range="*",
+    )])
 
-group = aws.ec2.SecurityGroup('web-sg',
-    description='Enable HTTP access',
-    ingress=[
-        { 'protocol': 'tcp', 'from_port': 22, 'to_port': 22, 'cidr_blocks': ['0.0.0.0/0'] },
-        { 'protocol': 'tcp', 'from_port': 80, 'to_port': 80, 'cidr_blocks': ['0.0.0.0/0'] },
-    ],
-    egress=[
-        {
-            "protocol": "-1",
-            "from_port": 0,
-            "to_port": 0,
-            "cidr_blocks": ["0.0.0.0/0"],
-    }
-    ], 
-    vpc_id=virtualprivatecloud.id
- )
+#Vnets and subnets
+vnet = network.VirtualNetwork(
+    prefix_name+"-vnet",
+    address_space=network.AddressSpaceArgs(
+        address_prefixes=[vnet_ip_range],
+    ),
+    resource_group_name=resource_group.name,
+    virtual_network_name=(prefix_name+"-vnet"))
 
-server = aws.ec2.Instance('web-server',
-    ami='ami-08d4ac5b634553e16',
-    instance_type='t2.micro',
-    key_name='id',
-    vpc_security_group_ids=[group.id],# reference the security group resource above
-    subnet_id=publicsubnet.id,
- )
+vm_subnet = network.Subnet(
+    "vm-subnet",
+    address_prefix=vm_ip_range,
+    resource_group_name=resource_group.name,
+    subnet_name="vm-subnet",
+    virtual_network_name=vnet.name,
+    network_security_group=network.NetworkSecurityGroupArgs(
+        id=nsg.id
+    ))
 
-rds_sg = aws.rds.SecurityGroup("rds_sg", 
-    ingress=[aws.rds.SecurityGroupIngressArgs(
-    cidr="10.0.0.0/24",
-    security_group_id=[group.id],
-    security_group_name=[group.name],
-    )]
+#virtual Machine
+pip = network.PublicIPAddress(
+    resource_name=(prefix_name+"-pip"),
+    public_ip_address_name=(prefix_name+"-pip"),
+    resource_group_name=resource_group.name
 )
 
-
-rds_server = aws.rds.Instance("db-server",
-    allocated_storage=10,
-    engine="mysql",
-    engine_version="5.7",
-    instance_class="db.t3.micro",
-    db_name="mydb",
-    parameter_group_name="default.mysql5.7",
-    password="database",
-    skip_final_snapshot=True,
-    username="database",
-    vpc_security_group_ids=[rds_sg.id],
+nic = network.NetworkInterface(
+    resource_name=(prefix_name+"-nic"),
+    ip_configurations=[network.NetworkInterfaceIPConfigurationArgs(
+        name="ipconfig1",
+        public_ip_address=network.PublicIPAddressArgs(
+            id=pip.id,
+        ),
+        subnet=network.SubnetArgs(
+            id=vm_subnet.id,
+        ),
+    )],
+    network_interface_name=(prefix_name+"-nic"),
+    resource_group_name=resource_group.name
 )
 
-bucket = aws.s3.Bucket("bucket",
-   acl="public-read",
-   tags={
-        "Environment": "Dev",
-        "Name": "My bucket",
-    })
+vm = compute.VirtualMachine(
+    resource_name=(prefix_name + "-vm"),
+    hardware_profile=compute.HardwareProfileArgs(
+        vm_size="Standard_B1s",
+    ),
+    network_profile=compute.NetworkProfileArgs(
+    network_interfaces=[compute.NetworkInterfaceReferenceArgs(
+            id=nic.id,
+            primary=True,
+        )],
+    ),
+    os_profile=compute.OSProfileArgs(
+        admin_username="global",
+        computer_name=(prefix_name + "-vm"),
+        linux_configuration=compute.LinuxConfigurationArgs(
+            disable_password_authentication=True,
+            ssh=compute.SshConfigurationArgs(
+                public_keys=[compute.SshPublicKeyArgs(
+                    key_data = ssh_key,
+                    path     = "/home/global/.ssh/authorized_keys",
+                )],
+            ),
+        ),
+    ),
+    resource_group_name=resource_group.name,
+    storage_profile=compute.StorageProfileArgs(
+        image_reference=compute.ImageReferenceArgs(
+            offer="UbuntuServer",
+            publisher="Canonical",
+            sku="18.04-LTS",
+            version="latest",
+        ),
+        os_disk=compute.OSDiskArgs(
+            caching="ReadWrite",
+            create_option="FromImage",
+            managed_disk=compute.ManagedDiskParametersArgs(
+                storage_account_type="StandardSSD_LRS",
+            ),
+            name=(prefix_name + "-vm-osdisk"),
+        ),
+    ),
+    vm_name=(prefix_name + "-vm")
+)
+# Create an Azure resource (Storage Account)
+account = storage.StorageAccount('sa',
+    resource_group_name=resource_group.name,
+    sku=storage.SkuArgs(
+        name=storage.SkuName.STANDARD_LRS,
+    ),
+    kind=storage.Kind.STORAGE_V2)
+
+# Export the primary key of the Storage Account
+primary_key = pulumi.Output.all(resource_group.name, account.name) \
+    .apply(lambda args: storage.list_storage_account_keys(
+        resource_group_name=args[0],
+        account_name=args[1]
+    )).apply(lambda accountKeys: accountKeys.keys[0].value)
+    
+#mysql server
+server = azure_native.sql.Server("server",
+    administrator_login="pulumi@srivastavajatin12gmail.onmicrosoft.com",
+    administrator_login_password="Kola93418",
+    administrators=azure_native.sql.ServerExternalAdministratorArgs(
+        azure_ad_only_authentication=True,
+        login="srivastava.jatin12@gmail.com",
+        principal_type="User",
+        sid=" 6d092d73-9ba1-43d6-b310-f019ac9350b3",
+        tenant_id="22e5336b-2e4c-4414-8596-01e25584aaba",
+    ),
+    location="West US",
+    resource_group_name="pulumiAKS-rgs",
+    server_name="mysql-test")
+
+#database
+database = azure_native.sql.Database("database",
+    database_name="mydb",
+    location="West US",
+    resource_group_name="pulumiAKS-rgs",
+    server_name="mysql-test",
+    sku=azure_native.sql.SkuArgs(
+        name="S0",
+        tier="Standard",
+    ),
+     source_database_id="/subscriptions/861c7352-89aa-43cf-83c7-5e655de5127d/resourceGroups/pulumiAKS-rgs/providers/Microsoft.Sql/servers/mysql-test/databases/testdb")
 
 
-pulumi.export("vpcId", vpc.vpc_id)
-pulumi.export("publicSubnetIds", vpc.public_subnet_ids)
-pulumi.export("privateSubnetIds", vpc.private_subnet_ids)    
-pulumi.export('bucket_name',  bucket.id)
-pulumi.export('public_ip_db', default.public_id)
-pulumi.export('public_dns_db', default.public_dns)
-pulumi.export('public_ip', server.public_ip)
-pulumi.export('public_dns', server.public_dns)   
+pulumi.export("primary_storage_key", primary_key)
+                                                   
+                                                                                                                                                                     
 
+
+                                                                                                                                                                
 
 
 
